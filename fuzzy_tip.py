1374016db7d006033c65c3cdacb93c0efc88a092
Shnuer <SAU@SAU.ru>
2019-03-28 17:09:12 +0300

    new commit

--- fuzzy_tip.py
+++ fuzzy_tip.py
@@ -51,9 +51,9 @@ for ax in (ax0, ax1, ax2):
 
 plt.tight_layout()
 
-# # We need the activation of our fuzzy membership functions at these values.
-# # The exact values 6.5 and 9.8 do not exist on our universes...
-# # This is what fuzz.interp_membership exists for!
+# We need the activation of our fuzzy membership functions at these values.
+# The exact values 6.5 and 9.8 do not exist on our universes...
+# This is what fuzz.interp_membership exists for!
 # qual_level_lo = fuzz.interp_membership(x_qual, qual_lo, 6.5)
 # qual_level_md = fuzz.interp_membership(x_qual, qual_md, 6.5)
 # qual_level_hi = fuzz.interp_membership(x_qual, qual_hi, 6.5)
@@ -62,66 +62,76 @@ plt.tight_layout()
 # serv_level_md = fuzz.interp_membership(x_serv, serv_md, 9.8)
 # serv_level_hi = fuzz.interp_membership(x_serv, serv_hi, 9.8)
 
-# # Now we take our rules and apply them. Rule 1 concerns bad food OR service.
-# # The OR operator means we take the maximum of these two.
-# active_rule1 = np.fmax(qual_level_lo, serv_level_lo)
+qual_level_lo = fuzz.interp_membership(x_qual, qual_lo, 0)
+qual_level_md = fuzz.interp_membership(x_qual, qual_md, 0)
+qual_level_hi = fuzz.interp_membership(x_qual, qual_hi, 0)
 
-# # Now we apply this by clipping the top off the corresponding output
-# # membership function with `np.fmin`
-# tip_activation_lo = np.fmin(active_rule1, tip_lo)  # removed entirely to 0
+serv_level_lo = fuzz.interp_membership(x_serv, serv_lo, 0)
+serv_level_md = fuzz.interp_membership(x_serv, serv_md, 0)
+serv_level_hi = fuzz.interp_membership(x_serv, serv_hi, 0)
 
-# # For rule 2 we connect acceptable service to medium tipping
-# tip_activation_md = np.fmin(serv_level_md, tip_md)
 
-# # For rule 3 we connect high service OR high food with high tipping
-# active_rule3 = np.fmax(qual_level_hi, serv_level_hi)
-# tip_activation_hi = np.fmin(active_rule3, tip_hi)
-# tip0 = np.zeros_like(x_tip)
+# Now we take our rules and apply them. Rule 1 concerns bad food OR service.
+# The OR operator means we take the maximum of these two.
+active_rule1 = np.fmax(qual_level_lo, serv_level_lo)
 
-# # Visualize this
-# fig, ax0 = plt.subplots(figsize=(8, 3))
+# Now we apply this by clipping the top off the corresponding output
+# membership function with `np.fmin`
+tip_activation_lo = np.fmin(active_rule1, tip_lo)  # removed entirely to 0
 
-# ax0.fill_between(x_tip, tip0, tip_activation_lo, facecolor='b', alpha=0.7)
-# ax0.plot(x_tip, tip_lo, 'b', linewidth=0.5, linestyle='--', )
-# ax0.fill_between(x_tip, tip0, tip_activation_md, facecolor='g', alpha=0.7)
-# ax0.plot(x_tip, tip_md, 'g', linewidth=0.5, linestyle='--')
-# ax0.fill_between(x_tip, tip0, tip_activation_hi, facecolor='r', alpha=0.7)
-# ax0.plot(x_tip, tip_hi, 'r', linewidth=0.5, linestyle='--')
-# ax0.set_title('Output membership activity')
+# For rule 2 we connect acceptable service to medium tipping
+tip_activation_md = np.fmin(serv_level_md, tip_md)
 
-# # Turn off top/right axes
-# for ax in (ax0,):
-#     ax.spines['top'].set_visible(False)
-#     ax.spines['right'].set_visible(False)
-#     ax.get_xaxis().tick_bottom()
-#     ax.get_yaxis().tick_left()
+# For rule 3 we connect high service OR high food with high tipping
+active_rule3 = np.fmax(qual_level_hi, serv_level_hi)
+tip_activation_hi = np.fmin(active_rule3, tip_hi)
+tip0 = np.zeros_like(x_tip)
 
-# plt.tight_layout()
+# Visualize this
+fig, ax0 = plt.subplots(figsize=(8, 3))
 
-# # Aggregate all three output membership functions together
-# aggregated = np.fmax(tip_activation_lo,
-#                      np.fmax(tip_activation_md, tip_activation_hi))
+ax0.fill_between(x_tip, tip0, tip_activation_lo, facecolor='b', alpha=0.7)
+ax0.plot(x_tip, tip_lo, 'b', linewidth=0.5, linestyle='--', )
+ax0.fill_between(x_tip, tip0, tip_activation_md, facecolor='g', alpha=0.7)
+ax0.plot(x_tip, tip_md, 'g', linewidth=0.5, linestyle='--')
+ax0.fill_between(x_tip, tip0, tip_activation_hi, facecolor='r', alpha=0.7)
+ax0.plot(x_tip, tip_hi, 'r', linewidth=0.5, linestyle='--')
+ax0.set_title('Output membership activity')
 
-# # Calculate defuzzified result
-# tip = fuzz.defuzz(x_tip, aggregated, 'centroid')
-# tip_activation = fuzz.interp_membership(x_tip, aggregated, tip)  # for plot
+# Turn off top/right axes
+for ax in (ax0,):
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.get_xaxis().tick_bottom()
+    ax.get_yaxis().tick_left()
 
-# # Visualize this
-# fig, ax0 = plt.subplots(figsize=(8, 3))
+plt.tight_layout()
 
-# ax0.plot(x_tip, tip_lo, 'b', linewidth=0.5, linestyle='--', )
-# ax0.plot(x_tip, tip_md, 'g', linewidth=0.5, linestyle='--')
-# ax0.plot(x_tip, tip_hi, 'r', linewidth=0.5, linestyle='--')
-# ax0.fill_between(x_tip, tip0, aggregated, facecolor='Orange', alpha=0.7)
-# ax0.plot([tip, tip], [0, tip_activation], 'k', linewidth=1.5, alpha=0.9)
-# ax0.set_title('Aggregated membership and result (line)')
+# Aggregate all three output membership functions together
+aggregated = np.fmax(tip_activation_lo,
+                     np.fmax(tip_activation_md, tip_activation_hi))
 
-# # Turn off top/right axes
-# for ax in (ax0,):
-#     ax.spines['top'].set_visible(False)
-#     ax.spines['right'].set_visible(False)
-#     ax.get_xaxis().tick_bottom()
-#     ax.get_yaxis().tick_left()
+# Calculate defuzzified result
+tip = fuzz.defuzz(x_tip, aggregated, 'centroid')
+tip_activation = fuzz.interp_membership(x_tip, aggregated, tip)  # for plot
 
-# plt.tight_layout()
+# Visualize this
+fig, ax0 = plt.subplots(figsize=(8, 3))
 
+ax0.plot(x_tip, tip_lo, 'b', linewidth=0.5, linestyle='--', )
+ax0.plot(x_tip, tip_md, 'g', linewidth=0.5, linestyle='--')
+ax0.plot(x_tip, tip_hi, 'r', linewidth=0.5, linestyle='--')
+ax0.fill_between(x_tip, tip0, aggregated, facecolor='Orange', alpha=0.7)
+ax0.plot([tip, tip], [0, tip_activation], 'k', linewidth=1.5, alpha=0.9)
+ax0.set_title('Aggregated membership and result (line)')
+
+# Turn off top/right axes
+for ax in (ax0,):
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.get_xaxis().tick_bottom()
+    ax.get_yaxis().tick_left()
+
+plt.tight_layout()
+
+plt.show()
